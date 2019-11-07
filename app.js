d230ffb8d6334086c49c089ebd1a86ec708b9908
abon <formygithubacc@gmail.com>
2019-11-07 17:56:39 +0500

    added valid JS questions with three oprions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ["B", "B", "B", "B"];
+const correctAnswers = ["C", "B", "B", "C", "A"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 
